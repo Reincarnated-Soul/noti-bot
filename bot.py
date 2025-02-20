37ad00bc95221be28f098fb1909454f8ab94e95d
Rajat Sahu <67316346+Coolboyrajat@users.noreply.github.com>
2025-02-20 23:27:16 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -251,7 +251,12 @@ async def main():
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        print("Bot stopped gracefully")
+    # try:
+    #     asyncio.run(main())
+    # except (KeyboardInterrupt, SystemExit):
+    #     print("Bot stopped gracefully")
+    # loop = asyncio.get_event_loop()
+    if loop.is_running():
+        loop.create_task(main())
+    else:
+        loop.run_until_complete(main())
